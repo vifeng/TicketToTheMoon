4f6b42aa8c5f3477c6783e172debfea80c0a76ad
Vifeng <virgvm@gmail.com>
2022-08-29 16:37:08 +0200

    WIP: - Cors error - lombok error

--- tickettothemoon_client/src/App.vue
+++ tickettothemoon_client/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import Employees from './components/Employees.vue'
 </script>
 
 <template>
@@ -13,7 +14,11 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
-    <TheWelcome />
+    <div id="app">
+      
+      <Employees/>
+    </div>
+    <!-- <TheWelcome /> -->
   </main>
 </template>
 
