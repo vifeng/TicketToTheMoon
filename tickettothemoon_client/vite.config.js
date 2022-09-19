955e87812127c42aa5b9a73d08de08d371e57cc8
Vifeng <virgvm@gmail.com>
2022-09-19 22:13:07 +0200

    WIP: proxy setup, still CORS issue

--- tickettothemoon_client/vite.config.js
+++ tickettothemoon_client/vite.config.js
@@ -1,8 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://cli.vuejs.org/config/#vue-config-js
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -10,11 +10,38 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        // target: 'http://localhost:5173',
+        target: 'http://localhost:8080',
+        ws: true,
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, '')
+      },
+      overlay: {
+        warnings: true,
+        errors: true
+      }
+    },
+  },
+  server: {
+    //   port: '8081',
+    // cors: false,
+    proxy: {
+      '/api': {
+        // target: 'http://localhost:5173',
+        target: 'http://localhost:8080',
+        ws: true,
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, '')
+      },
+    },
+  },
+  // Change build paths to make them Gradle compatible
+  // see https://cli.vuejs.org/config/
+  outputDir: 'build/dist'
 
-  // ,
-  // server:{
-  //   port: '8081',
-  //   cors: false
-  // }
-})
+});
+
