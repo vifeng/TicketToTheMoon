428dd298c858a2af3165b0165e72479e776affb3
Vifeng <22158753+vifeng@users.noreply.github.com>
2024-03-08 11:46:01 +0100

    Add Vite dependencies and update AppLink.vue
    This commit adds the following Vite dependencies to the project:
    - pinia.js
    - vue-router.js
    - vue.js
    These dependencies are required for the project to function properly.
    Format AppLink.vue

--- eventHub-site/src/components/AppLink.vue
+++ eventHub-site/src/components/AppLink.vue
@@ -1,28 +1,19 @@
 <template>
   <!-- external link -->
-  <a
-    v-if="isExternal"
-    target="_blank"
-    rel="noopener"
-    class="external-link"
-    :href="to"
-    ><slot
-  /></a>
+  <a v-if="isExternal" target="_blank" rel="noopener" class="external-link" :href="to"><slot /></a>
   <!-- internal link -->
-  <router-link v-else v-bind="$props" class="internal-link"
-    ><slot
-  /></router-link>
+  <router-link v-else v-bind="$props" class="internal-link"><slot /></router-link>
 </template>
 <script>
-import { RouterLink } from 'vue-router';
+import { RouterLink } from 'vue-router'
 export default {
   props: {
-    ...RouterLink.props,
+    ...RouterLink.props
   },
   computed: {
     isExternal() {
-      return typeof this.to === 'string' && this.to.startsWith('http');
-    },
-  },
-};
+      return typeof this.to === 'string' && this.to.startsWith('http')
+    }
+  }
+}
 </script>
