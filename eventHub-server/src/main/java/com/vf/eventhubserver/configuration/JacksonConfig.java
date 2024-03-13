f6c13025d2d297cc1dd2ca7b8bd9842c31a2e089
Vifeng <22158753+vifeng@users.noreply.github.com>
2024-03-13 15:12:07 +0100

    Refactor JacksonConfig
    Refactor Jackson configuration to let Spring Boot auto-configure ObjectMapper to avoid starting from scratch.

--- eventHub-server/src/main/java/com/vf/eventhubserver/configuration/JacksonConfig.java
+++ eventHub-server/src/main/java/com/vf/eventhubserver/configuration/JacksonConfig.java
@@ -1,38 +1,29 @@
 package com.vf.eventhubserver.configuration;
 
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
 @Configuration
 public class JacksonConfig {
 
+    private static final String DATEFORMAT = "dd-MM-yyyy";
+    private static final String DATETIMEFORMAT = DATEFORMAT + " HH:mm:ss";
+
     @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
 
-        // Configuration pour ignorer les propriétés inconnues dans la désérialisation
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-        // Configurez l'inclusion des valeurs NON NULL. permet de globaliser l'annotation suivante à
-        // toutes les entités
-        // @JsonInclude(JsonInclude.Include.NON_NULL) // Pour ignorer les champs null lors de la
-        // sérialisation
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-
-        // Configuration pour formater la sortie JSON de manière lisible
-        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
-
-        // Configuration pour LocalDate, LocalTime, Instant, etc.
-        objectMapper.registerModule(new JavaTimeModule());
-        // Configuration pour les Optional
-        objectMapper.registerModule(new Jdk8Module());
-
-        return objectMapper;
+    protected Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+        return new Jackson2ObjectMapperBuilder()
+                .serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(DATEFORMAT)))
+                .serializers(
+                        new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIMEFORMAT)))
+                .serializationInclusion(JsonInclude.Include.NON_NULL);
     }
+
 }
