091309f2a5d3cb74f4ee5a06d61dfd93a02e8cdd
Vifeng <22158753+vifeng@users.noreply.github.com>
2024-07-02 17:51:06 +0200

    Update ai chatbot with video

---
+++ event-hub-server/src/main/java/com/vf/eventhubserver/chatbot/VideoController.java
@@ -0,0 +1,36 @@
+package com.vf.eventhubserver.chatbot;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/chatbot")
+public class VideoController {
+    @GetMapping
+    public ResponseEntity<Resource> getVideo() {
+        try {
+            String filename = "static/video/chatbotCV.mp4";
+            Resource resource = new ClassPathResource(filename);
+
+            if (resource.exists() && resource.isReadable()) {
+                return ResponseEntity.ok()
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + resource.getFilename() + "\"")
+                        .contentType(MediaType.parseMediaType("video/mp4"))
+                        .body(resource);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
--- event-hub-site/src/components/TheChatBot.vue
+++ event-hub-site/src/components/TheChatBot.vue
@@ -1,34 +1,45 @@
 <template>
-  <div>
-    <h2>AI Chatbot</h2>
-    <h3>(on your right bottom end-side)</h3>
-    <p>
-      I created this chatbot using Dialogflow, a service on Google Cloud Platform, to help
-      recruiters hire me (Virginie Fengarol). It's trained on my info : CV, skills, and hobbies.
-      With its AI features, the chatbot understands questions about my background and dynamically
-      generates detailed insights, facilitating recruiters' decision-making process efficiently and
-      comprehensively. It also provides concise responses on my professional experience, education,
-      skills, and hobbies. Feel free to ask any questions!
-    </p>
-    <p>
-      It's still in development, so please use caution with the provided information and verify it
-      against my CV for accuracy.
-    </p>
-    <p>
-      <strong
-        >This chatbot needs credentials, and might not work. Please contact me for demo.</strong
-      >
-    </p>
+  <div class="container">
+    <div class="video-container">
+      <video width="600" controls class="video">
+        <source :src="videoUrl" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div class="text-container">
+      <h2>AI Chatbot</h2>
+      <h3>(on your right bottom end-side)</h3>
+      <p>
+        I created this chatbot using Dialogflow, a service on Google Cloud Platform, to help
+        recruiters hire me (Virginie Fengarol). It's trained on my info : CV, skills, and hobbies.
+        With its AI features, the chatbot understands questions about my background and dynamically
+        generates detailed insights, facilitating recruiters' decision-making process efficiently
+        and comprehensively. It also provides concise responses on my professional experience,
+        education, skills, and hobbies. Feel free to ask any questions!
+      </p>
+      <p>
+        It's still in development, so please use caution with the provided information and verify it
+        against my CV for accuracy.
+      </p>
+      <p>
+        <strong
+          >This chatbot needs credentials, and might not work. In the meantime, have a look a the
+          demo video.</strong
+        >
+      </p>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 
 const GCP_PROJECT_ID = import.meta.env.VITE_GCP_PROJECT_ID
 const CHATBOT_AGENT_ID = import.meta.env.VITE_CHATBOT_AGENT_ID
+const videoUrl = ref('')
 
 onMounted(() => {
+  videoUrl.value = 'http://localhost:8080/api/chatbot'
   const script = document.createElement('script')
   script.src =
     'https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js'
@@ -48,6 +59,7 @@ function initializeChatBot() {
   dfMessenger.setAttribute('language-code', 'en')
   dfMessenger.setAttribute('max-query-length', '-1')
 
+  
   const chatBubble = document.createElement('df-messenger-chat-bubble')
   chatBubble.setAttribute('chat-title', 'AI Chatbot for recruiter')
 
@@ -67,3 +79,24 @@ function initializeChatBot() {
   document.body.appendChild(dfMessenger)
 }
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: flex-start; 
+  gap: 20px; 
+}
+
+.video-container {
+  flex: 1; 
+}
+.video {
+  border: 4px solid #0957D0; /* Border around the video */
+  border-radius: 8px; /* Rounded corners */
+  box-shadow: -8px 8px 8px rgba(0, 0, 0, 0.1); /* Bottom-left shadow effect */
+  width: 60%; /* Ensure video takes full width of container */
+}
+.text-container {
+  flex: 1; 
+}
+</style>
