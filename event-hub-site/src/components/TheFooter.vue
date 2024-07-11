11f46412dd4886f8ce42dc76a47c8891588e6ebf
Vifeng <22158753+vifeng@users.noreply.github.com>
2024-07-11 11:35:35 +0200

    Fix footer
    remove deprecated sodipodi

--- event-hub-site/src/components/TheFooter.vue
+++ event-hub-site/src/components/TheFooter.vue
@@ -1,44 +1,29 @@
 <template>
     <div id="footer">
-        made with
-        <AppLink id="logoVuejs" to="https://vuejs.org/">
-            <svg alt="VueJs logo" class="logoVuejs" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 261.76 226.69">
-                <path d="M161.096.001l-30.225 52.351L100.647.001H-.005l130.877 226.688L261.749.001z" fill="#41b883" />
-                <path d="M161.096.001l-30.225 52.351L100.647.001H52.346l78.526 136.01L209.398.001z" fill="#34495e" />
-            </svg>
-        </AppLink> and&nbsp;
-        <AppLink id="logoSpring" alt="Spring Logo" to="https://spring.io/">
-            <svg class="logoSpring" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#"
-                xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg"
-                xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
-                xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" version="1.1" id="svg29" xml:space="preserve"
-                width="245.09332" height="62.986668" viewBox="0 0 245.09332 62.986668"
-                sodipodi:docname="PVLG-Spring-FullColor-Transparent.svg" inkscape:version="0.92.3 (unknown)">
-                <metadata id="metadata35">
-                    <rdf:RDF>
-                        <cc:Work rdf:about="">
-                            <dc:format>image/svg+xml</dc:format>
-                            <dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
-                            <dc:title></dc:title>
-                        </cc:Work>
-                    </rdf:RDF>
-                </metadata>
-                <defs id="defs33" />
-                <sodipodi:namedview pagecolor="#ffffff" bordercolor="#666666" borderopacity="1" objecttolerance="10"
-                    gridtolerance="10" guidetolerance="10" inkscape:pageopacity="0" inkscape:pageshadow="2"
-                    inkscape:window-width="1853" inkscape:window-height="1025" id="namedview31" showgrid="false"
-                    inkscape:zoom="6.8626921" inkscape:cx="122.54666" inkscape:cy="31.493334" inkscape:window-x="67"
-                    inkscape:window-y="27" inkscape:window-maximized="1" inkscape:current-layer="g37" />
-                <g id="g37" inkscape:groupmode="layer" inkscape:label="ink_ext_XXXXXX"
-                    transform="matrix(1.3333333,0,0,-1.3333333,0,62.986667)">
-                    <g id="g39" transform="scale(0.1)">
-                        <path
-                            d="M 429.32,132.246 C 371.109,54.6875 246.762,80.8672 167.039,77.1016 c 0,0 -14.113,-0.7852 -28.328,-3.125 0,0 5.375,2.2929 12.23,4.6562 55.993,19.3633 82.454,23.2542 116.477,40.7302 63.926,32.746 127.547,104.075 140.492,178.172 C 383.578,226.27 309.574,164.934 242.191,140.027 196.07,123.008 112.664,106.445 112.652,106.438 c 0.039,-0.059 -3.324,1.746 -3.371,1.785 -56.7224,27.597 -58.3591,150.449 44.66,190.05 45.168,17.375 88.372,7.832 137.2,19.454 52.078,12.382 112.394,51.418 136.91,102.453 27.484,-81.52 60.5,-209.059 1.269,-287.934 z m 1.012,313.195 c -6.93,-16.464 -15.387,-31.3 -25.137,-44.57 -42.914,44.043 -102.812,71.484 -169.019,71.484 C 105.941,472.355 0,366.402 0,236.191 0,167.93 29.1367,106.367 75.5898,63.2148 l 5.1719,-4.582 c -8.6367,7.043 -9.9648,19.7266 -2.9648,28.3711 7.0429,8.6484 19.7383,9.9883 28.3791,2.9844 8.664,-7.0195 9.984,-19.7344 2.969,-28.3945 -6.989,-8.6641 -19.7075,-9.9727 -28.3598,-2.9805 l 3.5195,-3.1172 C 125.395,20.8984 178.363,0 236.176,0 360.66,0 462.879,96.875 471.629,219.148 c 6.48,59.868 -11.203,135.887 -41.297,226.293"
-                            style="fill:#5fb832;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path55"
-                            inkscape:connector-curvature="0" />
-                    </g>
-                </g>
-            </svg>
-        </AppLink>
+      made with
+      <AppLink id="logoVuejs" to="https://vuejs.org/">
+        <svg class="logoVuejs" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 261.76 226.69">
+          <path d="M161.096.001l-30.225 52.351L100.647.001H-.005l130.877 226.688L261.749.001z" fill="#41b883" />
+          <path d="M161.096.001l-30.225 52.351L100.647.001H52.346l78.526 136.01L209.398.001z" fill="#34495e" />
+        </svg>
+      </AppLink>
+      and&nbsp;
+      <AppLink id="logoSpring" to="https://spring.io/">
+        <svg class="logoSpring" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 523.52 135.02" width="100%">
+          <!-- Adjusted viewBox and scaling to match Vue.js logo size -->
+          <g transform="scale(0.2)">
+            <path d="M 429.32,132.246 C 371.109,54.6875 246.762,80.8672 167.039,77.1016 c 0,0 -14.113,-0.7852 -28.328,-3.125 0,0 5.375,2.2929 12.23,4.6562 55.993,19.3633 82.454,23.2542 116.477,40.7302 63.926,32.746 127.547,104.075 140.492,178.172 C 383.578,226.27 309.574,164.934 242.191,140.027 196.07,123.008 112.664,106.445 112.652,106.438 c 0.039,-0.059 -3.324,1.746 -3.371,1.785 -56.7224,27.597 -58.3591,150.449 44.66,190.05 45.168,17.375 88.372,7.832 137.2,19.454 52.078,12.382 112.394,51.418 136.91,102.453 27.484,-81.52 60.5,-209.059 1.269,-287.934 z m 1.012,313.195 c -6.93,-16.464 -15.387,-31.3 -25.137,-44.57 -42.914,44.043 -102.812,71.484 -169.019,71.484 C 105.941,472.355 0,366.402 0,236.191 0,167.93 29.1367,106.367 75.5898,63.2148 l 5.1719,-4.582 c -8.6367,7.043 -9.9648,19.7266 -2.9648,28.3711 7.0429,8.6484 19.7383,9.9883 28.3791,2.9844 8.664,-7.0195 9.984,-19.7344 2.969,-28.3945 -6.989,-8.6641 -19.7075,-9.9727 -28.3598,-2.9805 l 3.5195,-3.1172 C 125.395,20.8984 178.363,0 236.176,0 360.66,0 462.879,96.875 471.629,219.148 c 6.48,59.868 -11.203,135.887 -41.297,226.293" style="fill:#5fb832;fill-opacity:1;fill-rule:nonzero;stroke:none" />
+          </g>
+        </svg>
+      </AppLink>
     </div>
-</template>
+  </template>
+<script>
+import AppLink from '@/components/AppLink.vue'; // Adjust the path to your AppLink component
+
+export default {
+  components: {
+    AppLink
+  }
+};
+</script>
