7a6d248843cae624fd76188f14bf84bd4a691054
Vifeng <22158753+vifeng@users.noreply.github.com>
2024-07-11 11:39:16 +0200

    Add dropdown menu to nav

--- event-hub-site/src/components/TheNavigation.vue
+++ event-hub-site/src/components/TheNavigation.vue
@@ -5,17 +5,93 @@
     /></AppLink>
     <AppLink to="/">Home</AppLink>
     <AppLink to="/about">About</AppLink>
-    <AppLink to="/employees">Employees</AppLink>
-    <AppLink to="/venues">Venues</AppLink>
+    <div class="dropdown" @mouseover="openDropdown" @mouseleave="closeDropdown">
+      <AppLink to="#">Entities</AppLink>
+      <div class="dropdown-content" v-if="isDropdownOpen">
+        <AppLink to="/employees">Employees</AppLink>
+        <AppLink to="/venues">Venues</AppLink>
+        <AppLink to="/customers">Customers</AppLink>
+        <AppLink to="/ticketReservation">ticketReservation</AppLink>
+
+      </div>
+    </div>
+    <AppLink to="/apiDoc">ApiDoc</AppLink>
     <AppLink to="/chatbot">AI chatbot</AppLink>
-    <!-- <AppLink :to="{ name: 'protected' }">Dashboard</AppLink> -->
-    <!-- this route will send you to the protected page if you login -->
   </div>
 </template>
-
-<script>
+<script setup>
+import { ref } from 'vue'
+import { useMouseInElement } from '@vueuse/core'
 import AppLink from './AppLink.vue'
-export default {
-  components: { AppLink }
+
+const isDropdownOpen = ref(false)
+
+function openDropdown() {
+  isDropdownOpen.value = true
+}
+
+function closeDropdown() {
+  isDropdownOpen.value = false
 }
 </script>
+
+<style scoped>
+#nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  margin-top: 0;
+  padding: 8px;
+  display: flex;
+  align-items: center;
+  border-bottom: 5px solid #4e6c71;
+  z-index: 2;
+  font-weight: bold;
+  background: #f0f1f6;
+}
+
+#nav a {
+  display: inline-block;
+  margin-right: 15px;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.2 ease color;
+}
+
+#nav a.AppLink-active,
+#nav a:hover {
+  opacity: 1;
+  font-size: x-large;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: hsla(160, 100%, 37%, 1);
+  padding: 8px 12px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:last-child {
+  padding: 8px 12px 25px 12px; 
+}
+.dropdown-content a:hover {
+  background-color: #f1f1f1;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+</style>
