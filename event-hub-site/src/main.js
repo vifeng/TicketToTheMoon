fde384cfedd6024da1d1530a75f0e9d906a2edec
Vifeng <22158753+vifeng@users.noreply.github.com>
2024-07-12 19:40:18 +0200

    Installation of vuetify and vueuse

--- event-hub-site/src/main.js
+++ event-hub-site/src/main.js
@@ -2,12 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import 'vuetify/styles'
+import vuetify from './plugins/vuetify'
 
 import App from './App.vue'
 import router from './router'
 import AppLink from './components/AppLink.vue'
-const app = createApp(App)
 
+const app = createApp(App)
+app.use(vuetify)
 app.component('AppLink', AppLink)
 app.use(createPinia())
 app.use(router)
---
+++ event-hub-site/src/plugins/vuetify.js
@@ -0,0 +1,21 @@
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@mdi/font/css/materialdesignicons.css'
+
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import { fa } from 'vuetify/iconsets/fa'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export default createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+      fa
+    }
+  }
+})
