24675394215328815de0364d177a60aadd2278e3
Vifeng <22158753+vifeng@users.noreply.github.com>
2024-09-11 16:57:25 +0200

    Update Customer view
    Add button and style
    text fix

--- event-hub-site/src/views/CustomerView.vue
+++ event-hub-site/src/views/CustomerView.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="Customer">
-    <h1>Employees</h1>
+    <h1>Customers</h1>
     <p>
       Sort by id
-      <button @click="sorting.sortIdByAscending">Sort by id ascending</button>
-      <button @click="sorting.sortIdByDescending">Sort by id descending</button>
+      <v-btn prepend-icon="mdi-arrow-collapse-up" @click="sorting.sortIdByAscending"
+        >Sort by id ascending</v-btn
+      >
+      <v-btn prepend-icon="mdi-arrow-collapse-down" @click="sorting.sortIdByDescending"
+        >Sort by id descending</v-btn
+      >
     </p>
     <p>
       Sort by Username
-      <button @click="sorting.sortUsernameByAscending">Sort by Username ascending</button>
-      <button @click="sorting.sortUsernameByDescending">Sort by Username descending</button>
+      <v-btn prepend-icon="mdi-arrow-collapse-up" @click="sorting.sortUsernameByAscending"
+        >Sort by Username ascending</v-btn
+      >
+      <v-btn prepend-icon="mdi-arrow-collapse-down" @click="sorting.sortUsernameByDescending"
+        >Sort by Username descending</v-btn
+      >
     </p>
     <div v-if="isLoading">Loading...</div>
     <div v-else class="customers__list">
@@ -44,3 +52,11 @@ onMounted(async () => {
 
 const sortedCustomers = sorting.items
 </script>
+
+<style scoped>
+.v-btn {
+  margin: 0 10px 10px 0;
+  background-color: #3fb883;
+  color: white;
+}
+</style>
