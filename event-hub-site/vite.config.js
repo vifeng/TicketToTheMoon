605d0a3b506291705ed3d0b574756546e8ed7b87
Vifeng <22158753+vifeng@users.noreply.github.com>
2024-09-18 21:04:51 +0200

    Add vite-plugin-inspect
    - vite-plugin-inspect provides a web-based interface to inspect and debug Vite’s internal state and configuration

--- event-hub-site/vite.config.js
+++ event-hub-site/vite.config.js
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
+import Inspect from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Components({})],
+  plugins: [vue(), Components({}), Inspect()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
